369b4f47f58c188162c233d41cbf9fd7840c8311
archanabotla08 <archana.botla@gmail.com>
2020-12-23 08:29:53 +0530

    [Archana] Added: UC1 to start with employee pay roll app and with welcome message

---
+++ src/main/java/com/blz/employeepayrollapp/EmployeePayRollAppSpringApplication.java
@@ -0,0 +1,14 @@
+package com.blz.employeepayrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayRollAppSpringApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Pay Roll App");
+		SpringApplication.run(EmployeePayRollAppSpringApplication.class, args);
+	}
+
+}
