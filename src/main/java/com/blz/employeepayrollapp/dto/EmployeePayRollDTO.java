2077d5aab0eb25c454f77dfe7153d7ac2fef76ff
archanabotla08 <archana.botla@gmail.com>
2021-01-09 11:16:08 +0530

    [Archana] added: UC10 to validate the Employee payroll fields afor name n salary

--- src/main/java/com/blz/employeepayrollapp/dto/EmployeePayRollDTO.java
+++ src/main/java/com/blz/employeepayrollapp/dto/EmployeePayRollDTO.java
@@ -1,9 +1,17 @@
 package com.blz.employeepayrollapp.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
 public class EmployeePayRollDTO {
 
+	@Pattern(regexp="^[A-Z]{1}[a-zA-Z\\s]{2,}$", message="Invalid Employee Name")
+	@NotEmpty(message="Employee Name Should not be null")
 	public String name;
+	@Min(value = 5000, message="Min Employee Wage should be more than 500 ")
 	public long salary;
+	
 	public EmployeePayRollDTO(String name, long salary) {
 		super();
 		this.name = name;
