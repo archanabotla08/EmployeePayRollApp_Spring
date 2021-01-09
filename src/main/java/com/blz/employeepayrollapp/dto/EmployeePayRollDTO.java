f2e5e99016368b15b9a45dbb5f0ce9429274d671
archanabotla08 <archana.botla@gmail.com>
2021-01-09 11:39:53 +0530

    [Archana] Added: UC13 to add remailing fields of employee payroll to model and DTO

--- src/main/java/com/blz/employeepayrollapp/dto/EmployeePayRollDTO.java
+++ src/main/java/com/blz/employeepayrollapp/dto/EmployeePayRollDTO.java
@@ -1,8 +1,13 @@
 package com.blz.employeepayrollapp.dto;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class EmployeePayRollDTO {
 
@@ -12,13 +17,15 @@ public class EmployeePayRollDTO {
 	@Min(value = 5000, message="Min Employee Wage should be more than 500 ")
 	public long salary;
 	
-	public EmployeePayRollDTO(String name, long salary) {
-		super();
-		this.name = name;
-		this.salary = salary;
-	}
-	@Override
-	public String toString() {
-		return "EmployeePayRollDTO [empName=" + name + ", salary=" + salary + "]";
-	}
+	public String gender;
+
+	@JsonFormat(pattern = "dd MMM yyyy")
+	public LocalDate startDate;
+
+	public String note;
+
+	public String profilePic;
+
+	public List<String> departments;
+
 }
--- src/main/java/com/blz/employeepayrollapp/model/EmployeePayRollData.java
+++ src/main/java/com/blz/employeepayrollapp/model/EmployeePayRollData.java
@@ -1,24 +1,41 @@
 package com.blz.employeepayrollapp.model;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import com.blz.employeepayrollapp.dto.EmployeePayRollDTO;
 import com.blz.employeepayrollapp.dto.ResponseDTO;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
+import lombok.Data;
+
+@Data
 public class EmployeePayRollData {
+	
 	private int employeeId;
 	private String name;
 	private long salary;
-	
-	public EmployeePayRollData() {
-		
-	}
 
-	public EmployeePayRollData(int employeeId, String name, long salary) {
-		super();
-		this.employeeId = employeeId;
-		this.name = name;
-		this.salary = salary;
-	}
+	public String gender;
 
+	@JsonFormat(pattern = "dd MMM yyyy")
+	public LocalDate startDate;
+
+	public String note;
+
+	public String profilePic;
+
+	public List<String> departments;
+
+	public void updateEmployeePayrollData(EmployeePayRollDTO employeePayrollDTO) {
+		this.name = employeePayrollDTO.name;
+		this.salary = employeePayrollDTO.salary;
+		this.gender = employeePayrollDTO.gender;
+		this.startDate = employeePayrollDTO.startDate;
+		this.note = employeePayrollDTO.note;
+		this.profilePic = employeePayrollDTO.profilePic;
+		this.departments = employeePayrollDTO.departments;
+	}
 	public EmployeePayRollData(int employeeId, EmployeePayRollDTO employeePayRollDTO) {
 		super();
 		this.employeeId = employeeId;
